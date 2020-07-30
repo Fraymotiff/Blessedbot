5ff2c658604466079215ae97c4d26f70fc11a369
Fraymotiff <68887087+Fraymotiff@users.noreply.github.com>
2020-07-29 23:34:42 -0300

    blessed

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require ('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('Blessed bot is now online!', () => {
+     //Code for the bot to do when starting up.
+     })
+
+const PREFIX ='!'
+
+
+
+client.once('ready', () => {
+     //Code for the bot to do when starting up.
+     })
+     console.log('Cursed image sender is now online!')
+   
+     client.on('message', (message) => {
+     const PREFIX = '!'
+     
+
+     if (message.content.startsWith (PREFIX + "blessed")) {
+        let imageNumber = Math.floor(Math.random()* 75) +1
+            message.channel.send ("Let's Give it everything we got! It's wholesome time! (or is it?)",  {files: ["./blessed/" + imageNumber + ".png"]} )
+            return; 
+              
+             
+             
+     }
+     });
+    
+    
+
+
+
+    client.login(process.env.token);
+
